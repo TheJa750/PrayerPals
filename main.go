876dbc5898ba470cca4934fc0fa535524ecee356
TheJa750 <jtgames514@gmail.com>
2025-07-23 09:58:19 -0400

    Added simple logging middleware

--- main.go
+++ main.go
@@ -6,26 +6,17 @@ import (
 	"time"
 
 	"github.com/TheJa750/PrayerPals/internal/handlers"
+	"github.com/TheJa750/PrayerPals/internal/middleware"
 
 	_ "github.com/lib/pq" // Import the PostgreSQL driver
 )
 
 func main() {
-	/*
-		godotenv.Load()
-		dbURL := os.Getenv("DB_URL")
-
-		db, err := sql.Open("postgres", dbURL)
-		if err != nil {
-			log.Fatalf("Error connecting to the database: %v", err)
-		}
-	*/
-
 	mux := http.NewServeMux()
 
 	svr := http.Server{
 		Addr:              ":8080",
-		Handler:           mux,
+		Handler:           middleware.LoggingMiddleware(mux),
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 	log.Println("Starting server on :8080")
@@ -37,3 +28,13 @@ func main() {
 
 	log.Fatal(svr.ListenAndServe())
 }
+
+/*
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+*/
